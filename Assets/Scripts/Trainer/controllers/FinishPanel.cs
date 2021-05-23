332695ffdc3b5921cbd5632813d61322e05f22ea
Алексей Парников <cyraxchel@list.ru>
2021-05-23 17:04:17 +0300

    Added time counter.

--- Assets/Scripts/Trainer/controllers/FinishPanel.cs
+++ Assets/Scripts/Trainer/controllers/FinishPanel.cs
@@ -1,26 +1,54 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-
+using TMPro;
 public class FinishPanel : MonoBehaviour
 {
 
     [SerializeField]
     GameObject panelRool;
-    [SerializeField]
-    Button continueButton;
+    
     [SerializeField]
     Button restartButton;
+    [SerializeField]
+    TextMeshProUGUI timeText;
+    [SerializeField]
+    TextMeshProUGUI errorCountText;
 
-    // Start is called before the first frame update
-    void Start() {
-        
+
+    private void Awake()
+    {
+        TrainSteps.TrainConfigurationComplete += Subscribelisteners;
+        restartButton.onClick.AddListener(RestartAction);
+        panelRool.SetActive(false);
+        timeText.text = "";
+        errorCountText.text = "";
+        panelRool.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void RestartAction()
     {
-        
+        SceneManager.LoadScene(0);
+    }
+
+
+    private void Subscribelisteners(TrainModel model)
+    {
+        model.TrainFinished += ShowStatistics;
+    }
+
+    private void ShowStatistics(int numErrors)
+    {
+
+        errorCountText.text = numErrors.ToString() ;
+        panelRool.SetActive(true);
+    }
+
+    public void SetTotalTimeText(string totaltime)
+    {
+        timeText.text = totaltime;
     }
 }
---
+++ Assets/Scripts/Trainer/controllers/TrainerTimer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TrainerTimer : MonoBehaviour
+{
+    public TimeReadyEvent TrainTimeReady;
+
+    float trainerStartTime = 0;
+    float trainerEndTime = 0;
+    public float TotalTime { get { return trainerEndTime - trainerStartTime; } }
+    public string TotalTimeFormat { 
+        get {
+            TimeSpan ts = TimeSpan.FromSeconds(TotalTime);
+            return string.Format("{0:D2}h:{1:D2}m:{2:D2}s", ts.Hours, ts.Minutes, ts.Seconds);
+        } 
+    }
+    // Start is called before the first frame update
+    void Awake()
+    {
+        TrainSteps.TrainConfigurationComplete += Subscribelisteners;
+    }
+
+    private void Subscribelisteners(TrainModel model)
+    {
+        model.TrainStart += FixStartTime;
+        model.TrainFinished += FixEndTime;
+    }
+
+    private void FixEndTime(int totaltime)
+    {
+        trainerEndTime = Time.timeSinceLevelLoad;
+
+        Debug.Log(trainerStartTime);
+        Debug.Log(trainerEndTime);
+
+        TrainTimeReady?.Invoke(TotalTimeFormat);
+    }
+
+    private void FixStartTime()
+    {
+        trainerStartTime = Time.timeSinceLevelLoad;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    [Serializable]
+    public class TimeReadyEvent : UnityEvent<string> { }
+}
