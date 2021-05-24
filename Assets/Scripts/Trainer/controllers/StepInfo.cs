fcf54e1889cc8bab8346a948156829b8e27ae3f4
Алексей Парников <cyraxchel@list.ru>
2021-05-24 10:37:49 +0300

    added second scene added text info about current step.

---
+++ Assets/Scripts/Trainer/controllers/StepInfo.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using cyraxchel.trainer.config;
+using System;
+
+namespace cyraxchel.trainer.controllers
+{
+
+    public class StepInfo : MonoBehaviour
+    {
+        [SerializeField]
+        TextMeshProUGUI infoField;
+        [SerializeField]
+        GameObject rootPanel;
+
+        // Start is called before the first frame update
+        void Awake()
+        {
+            TrainSteps.TrainConfigurationComplete += Subscribelisteners;
+            infoField.text = "";
+        }
+
+        private void Subscribelisteners(TrainModel model)
+        {
+            model.StepChanged += OnStepChanged;
+            model.TrainFinished += OnFinishTraining;
+        }
+
+        private void OnFinishTraining(int err)
+        {
+            rootPanel.SetActive(false);
+        }
+
+        private void OnStepChanged(Step step)
+        {
+            infoField.text = step.Label;
+        }
+
+        
+    }
+}
